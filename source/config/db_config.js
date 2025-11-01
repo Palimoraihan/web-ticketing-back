c218838840f51acb6a6667a30a8206b0396fc148
palimo <palimosrehans@gmail.com>
2025-11-01 08:02:36 +0700

    Update db config

--- source/config/db_config.js
+++ source/config/db_config.js
@@ -7,7 +7,13 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
+    port:process.env.DB_PORT,
     dialect: "mysql",
+    dialectOptions:{
+      ssl: {
+      require: true, // <<<<<<< YOU NEED THIS
+    }
+    },
     // logging: console.log
     logging: false
 

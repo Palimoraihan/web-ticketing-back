2f3d19531fca8785baf9e08eaef26b3bea0f7118
palimo <palimosrehans@gmail.com>
2025-11-01 09:33:33 +0700

    upload dir path resolve

--- src/controller/attachment_controller.js
+++ src/controller/attachment_controller.js
@@ -3,7 +3,7 @@ const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
 
-const uploadDir = "uploads";
+const uploadDir = "/tmp/uploads";
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
